b92a1599bd85a8cf6cecedbd0d1fb32d4115fbf8
Zachary Latta <zchlatta@gmail.com>
2013-04-26 20:30:56 -0700

    Changed desktop title

--- desktop/src/main/java/com/zachlatta/minimalistic_potato/java/MinimalisticPotatoDesktop.java
+++ desktop/src/main/java/com/zachlatta/minimalistic_potato/java/MinimalisticPotatoDesktop.java
@@ -5,10 +5,13 @@ import com.zachlatta.minimalistic_potato.core.MinimalisticPotato;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-public class MinimalisticPotatoDesktop {
-	public static void main(String[] args) {
+public class MinimalisticPotatoDesktop
+{
+	public static void main(String[] args)
+    {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.useGL20 = true;
+        config.title = "Minimalistic Potato";
 		new LwjglApplication(new MinimalisticPotato(), config);
 	}
 }
