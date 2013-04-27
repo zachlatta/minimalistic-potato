c6ce2b310c87a2cb140438c860d641008f2c0c5d
Zachary Latta <zchlatta@gmail.com>
2013-04-27 15:30:29 -0700

    Bigger dimensions for web.

--- html/src/main/java/com/zachlatta/minimalistic_potato/html/MinimalisticPotatoHtml.java
+++ html/src/main/java/com/zachlatta/minimalistic_potato/html/MinimalisticPotatoHtml.java
@@ -14,6 +14,6 @@ public class MinimalisticPotatoHtml extends GwtApplication {
 	
 	@Override
 	public GwtApplicationConfiguration getConfig () {
-		return new GwtApplicationConfiguration(480, 320);
+		return new GwtApplicationConfiguration(800, 640);
 	}
 }
