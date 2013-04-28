9c024f5610ca704c63a4ed0a436c3e7fc59b2007
Zachary Latta <zchlatta@gmail.com>
2013-04-27 18:17:46 -0700

    Revert "Revert "Potatoes be potatin'""
    This reverts commit 33286843695a5c3ba5345605123a75d6c638f9de.

--- core/src/main/java/com/zachlatta/minimalistic_potato/core/MinimalisticPotato.java
+++ core/src/main/java/com/zachlatta/minimalistic_potato/core/MinimalisticPotato.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
@@ -13,6 +14,8 @@ import com.zachlatta.minimalistic_potato.core.actor.Potato;
 
 public class MinimalisticPotato implements ApplicationListener
 {
+    private static final boolean KEEP_ASPECT_RATIO = false;
+
     private Stage stage;
     private Actor potato;
 
@@ -22,6 +25,7 @@ public class MinimalisticPotato implements ApplicationListener
         stage = new Stage();
         potato = new Potato();
 
+        // Center dat potato
         potato.setX(stage.getWidth() / 2 - potato.getWidth() / 2);
         potato.setY(stage.getHeight() / 2 - potato.getHeight() / 2);
 
@@ -54,16 +58,59 @@ public class MinimalisticPotato implements ApplicationListener
     @Override
     public void resize(int width, int height)
     {
+        stage.setViewport(width, height, KEEP_ASPECT_RATIO);
     }
 
     @Override
     public void render()
     {
-        Gdx.gl.glClearColor(255, 255, 255, 0);
+        Gdx.gl.glClearColor(MathUtils.randomBoolean() ? 1 : 0, MathUtils.randomBoolean() ? 1 : 0,
+                MathUtils.randomBoolean() ? 1 : 0, 0);
         Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
+
+        if(MathUtils.random() > .99)
+        {
+            final Actor actor = new Potato();
+
+            actor.setX(MathUtils.random(stage.getWidth() - actor.getWidth()));
+            actor.setY(MathUtils.random(stage.getHeight() - actor.getHeight()));
+
+            actor.addListener(new InputListener()
+            {
+                public boolean touchDown(InputEvent event, float x, float y, int pointer, int button)
+                {
+                    return true;
+                }
+
+                public void touchUp(InputEvent event, float x, float y, int pointer, int button)
+                {
+                    MoveToAction action = new MoveToAction();
+                    action.setPosition(x + actor.getX() - actor.getWidth() / 2,
+                            y + actor.getY() - actor.getHeight() / 2);
+                    action.setDuration(1);
+                    action.setInterpolation(Interpolation.exp5);
+                    actor.addAction(action);
+                }
+            });
+
+            stage.addActor(actor);
+        }
+
+        for (final Actor actor : stage.getActors())
+        {
+            if(actor.getActions().size == 0)
+            {
+                MoveToAction action = new MoveToAction();
+                action.setPosition(MathUtils.random(stage.getWidth() - actor.getWidth()),
+                        MathUtils.random(stage.getHeight() - actor.getHeight()));
+                action.setDuration(.5f);
+                action.setInterpolation(Interpolation.exp5);
+                actor.addAction(action);
+            }
+        }
     }
 
     @Override
