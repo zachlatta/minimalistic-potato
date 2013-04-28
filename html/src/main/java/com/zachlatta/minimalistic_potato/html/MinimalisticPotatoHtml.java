7223d8c9fe980128040179dc08cc7a72e8107329
Zachary Latta <zchlatta@gmail.com>
2013-04-27 18:17:48 -0700

    Revert "HTML formatting."
    This reverts commit b1cb8fd2ad6a92b4da5d4fea8b57d24fff232f65.

--- html/src/main/java/com/zachlatta/minimalistic_potato/html/MinimalisticPotatoHtml.java
+++ html/src/main/java/com/zachlatta/minimalistic_potato/html/MinimalisticPotatoHtml.java
@@ -6,17 +6,14 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 
-public class MinimalisticPotatoHtml extends GwtApplication
-{
+public class MinimalisticPotatoHtml extends GwtApplication {
 	@Override
-	public ApplicationListener getApplicationListener ()
-    {
+	public ApplicationListener getApplicationListener () {
 		return new MinimalisticPotato();
 	}
 	
 	@Override
-	public GwtApplicationConfiguration getConfig ()
-    {
+	public GwtApplicationConfiguration getConfig () {
 		return new GwtApplicationConfiguration(800, 640);
 	}
 }
